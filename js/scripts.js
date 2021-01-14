4be8aff37ecf4de32105409bd14201b6aefd3f64
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-01-14 15:15:08 -0800

    Add js business logic
    Co-authored-by: David Couch <dcouch440@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
-  $("button.btn").submit(function() {
-    const sideA = $("input#sideA").val();
-    const sideB = $("input#sideB").val();
-    const sideC = $("input#sideC").val();
-    // sideA === sideB && sideA === sideC
-    if (true) {
-      $("#triangle").text("an equilateral");
+  $("button.btn").click(function(event) {
+    const sideA = parseFloat($("input#sideA").val());
+    const sideB = parseFloat($("input#sideB").val());
+    const sideC = parseFloat($("input#sideC").val());
+    
+    if (!sideA && !sideB && !sideC) {
+      alert("Please make sure all fields contain a value > 0");
+    } else if (sideA === sideB && sideB === sideC) {
+      $("#answer").text("Your triangle is an equilateral triangle.");
     } else if (false) {
-      $("#triangle").text("an isosceles");
+      $("#answer").text("Your triangle is an isosceles triangle.");
     } else if (false) {
-      $("#triangle").text("a scalene");
-    } else {
-      $("#triangle").text("Sorry, we don't know what this is.");
+      $("#answer").text("Your triangle is a scalene triangle.");
+    } else if (false) {
+      $("#answer").text("Sorry, this is not a triangle.");
     }
+    
+    event.preventDefault();
   });
 });
