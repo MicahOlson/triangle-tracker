55a54bd4ec7eded1d3a38d048527f23985777f33
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-01-14 16:14:38 -0800

    Add logical operator comparisons to conditional statements
    Co-authored-by: David Couch <dcouch440@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,20 @@
 $(document).ready(function() {
-  $("button.btn").click(function(event) {
+  $("#formOne").submit(function(event) {
     const sideA = parseFloat($("input#sideA").val());
     const sideB = parseFloat($("input#sideB").val());
     const sideC = parseFloat($("input#sideC").val());
-    
+
     if (!sideA && !sideB && !sideC) {
-      alert("Please make sure all fields contain a value > 0");
-    } else if (sideA === sideB && sideB === sideC) {
-      $("#answer").text("Your triangle is an equilateral triangle.");
-    } else if (false) {
-      $("#answer").text("Your triangle is an isosceles triangle.");
-    } else if (false) {
-      $("#answer").text("Your triangle is a scalene triangle.");
-    } else if (false) {
-      $("#answer").text("Sorry, this is not a triangle.");
+        alert("Please make sure all fields contain a value > 0");
+    } else if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideC + sideB <= sideA) {
+        $("#answer").text("Sorry, this is not a triangle.");
+    } else if (sideA == sideB && sideB == sideC ) {
+        $("#answer").text("Your triangle is an equalateral triangle.");
+    } else if (sideA == sideB || sideB == sideC || sideC == sideA ) {
+        $("#answer").text("Your triangle is an isosceles triangle.");
+    } else if (sideA !== sideB && sideB !== sideC ) {
+        $("#answer").text("Your triangle is a scalene triangle.");
     }
-    
     event.preventDefault();
   });
 });
